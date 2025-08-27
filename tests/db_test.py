6b5eb1b0eea558cd2c9a3ed4a66e9ac7b3fe722d
eerla <gurubrahmam443@gmail.com>
2025-08-26 23:46:28 -0400

    postgres details in .env

--- tests/db_test.py
+++ tests/db_test.py
@@ -1,13 +1,16 @@
 import psycopg2
+import os
+import dotenv
 
+dotenv.load_dotenv()
 # Update these with your actual credentials
 conn = psycopg2.connect(
-    host="pg-tribevibe-tribevibe.b.aivencloud.com",
-    port=28769,
-    user="avnadmin",
-    password="AVNS_hHzR9EDrlesC9fjCo7Y",
-    dbname="tribevibe",
-    sslmode="require"
+    host=os.getenv("PGDB_HOST", "pg-tribevibe-tribevibe.b.aivencloud.com"),
+    port=os.getenv("PGDB_PORT", 28769),
+    user=os.getenv("PGDB_USER", "avnadmin"),
+    password=os.getenv("PGDB_PASSWORD", "set password in .env file"),
+    dbname=os.getenv("PGDB_NAME", "tribevibe"),
+    sslmode=os.getenv("PGDB_SSLMODE", "require")
 )
 print("Connected to the database successfully.")
 cur = conn.cursor()
